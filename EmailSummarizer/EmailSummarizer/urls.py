39599016ae3dec730af3eefa73e8483c1b9e620e
hsprabhakar <saurab@ualberta.ca>
2024-11-16 01:18:42 -0700

    nonfunctional but existing frontend
    literally just do npm run dev and go to /home

--- EmailSummarizer/EmailSummarizer/urls.py
+++ EmailSummarizer/EmailSummarizer/urls.py
@@ -20,7 +20,7 @@ from app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('auth/', views.oauth2_login, name='oauth2_login'), # Initiates OAuth flow
+    path('login/', views.oauth2_login, name='oauth2_login'), # Initiates OAuth flow
     path('oauth2callback/', views.oauth2_callback, name='oauth2_callback'),  # Handles redirect
     path('', views.home, name='home'),  # New home view for the root URL
 ]
---
+++ frontend/pages/home.tsx
@@ -0,0 +1,9 @@
+import { LoginCard } from "@/components/LoginCard";
+
+export default function Home() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <LoginCard />
+        </div>
+    );
+}
